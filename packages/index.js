9186e64d8264a13f63e6d5fc14fec3bd1e8bbf19
xxl <xiexiaoliang@fissoft.com>
2020-09-28 16:51:09 +0800

    fix export bug

--- packages/index.js
+++ packages/index.js
@@ -1,11 +1,12 @@
-export { default as CheckboxGroup } from './CheckboxGroup';
-export { default as InputLable } from './InputLable';
-export { default as InputRadio } from './InputRadio';
-export { default as InputRange } from './InputRange';
-export { default as InputTag } from './InputTag'
-export { default as SearchTable } from './SearchTable'
-export { default as SearchToolBar } from './SearchToolBar'
-export { default as GlobalModal } from './GlobalModal'
-export { default as utils } from './Utils/modelUtil'
-
+import CheckboxGroup from './CheckboxGroup';
+import InputLable from './InputLable';
+import InputRadio from './InputRadio';
+import InputRange from './InputRange';
+import InputTag from './InputTag'
+import SearchTable from './SearchTable'
+import SearchToolBar from './SearchToolBar'
+import GlobalModal from './GlobalModal'
+import utils from './Utils/modelUtil'
+ 
+export default { CheckboxGroup, InputLable, InputRadio, InputRange, InputTag, SearchTable, SearchToolBar, GlobalModal, utils };
 
