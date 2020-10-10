6b452b75de15ce9d52d803a6dcd71eeb8740b61c
xxl <xiexiaoliang@fissoft.com>
2020-10-10 16:18:25 +0800

    init

---
+++ .umirc.js
@@ -0,0 +1,84 @@
+import { readdirSync } from 'fs';
+import chalk from 'chalk';
+import { join } from 'path';
+
+const headPkgList = [];
+// utils must build before core
+// runtime must build before renderer-react
+const pkgList = readdirSync(join(__dirname, 'packages')).filter(
+  (pkg) => pkg.charAt(0) !== '.' && !headPkgList.includes(pkg),
+);
+
+const alias = pkgList.reduce((pre, pkg) => {
+  pre[`@ant-design/pro-${pkg}`] = join(__dirname, 'packages', pkg, 'src');
+  return {
+    ...pre,
+  };
+}, {});
+
+console.log(`🌼 alias list \n${chalk.blue(Object.keys(alias).join('\n'))}`);
+
+const tailPkgList = pkgList
+  .map((path) => [join('packages', path, 'src')])
+  .reduce((acc, val) => acc.concat(val), []);
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default {
+  title: 'ProComponents',
+  mode: 'site',
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
+  alias,
+  resolve: { includes: [...tailPkgList] },
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/ArvinXie/antd-components.git',
+    },
+  ],
+  analytics: false,
+  hash: true,
+  // dynamicImport: {
+  //   loading: '@ant-design/pro-skeleton',
+  // },
+  externals:
+    process.env.NODE_ENV === 'development'
+      ? {
+          react: 'window.React',
+          'react-dom': 'window.ReactDOM',
+          moment: 'window.moment',
+          antd: 'window.antd',
+        }
+      : {},
+  targets: {
+    chrome: 80,
+    firefox: false,
+    safari: false,
+    edge: false,
+    ios: false,
+  },
+  links:
+    process.env.NODE_ENV === 'development'
+      ? ['https://gw.alipayobjects.com/os/lib/antd/4.6.6/dist/antd.css']
+      : [],
+  scripts:
+    process.env.NODE_ENV === 'development'
+      ? [
+          'https://gw.alipayobjects.com/os/lib/react/16.13.1/umd/react.development.js',
+          'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.development.js',
+          'https://gw.alipayobjects.com/os/lib/moment/2.29.0/min/moment-with-locales.js',
+          'https://gw.alipayobjects.com/os/lib/antd/4.6.6/dist/antd-with-locales.js',
+        ]
+      : [],
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# antd-components
+> 对Antd组件进行二次封装和扩展，对业务系统更加友好
+
+## Install 
+```js
+npm install antd-component 
+ // or
+yarn add antd-component
+```
+
+## Antd Version
+4.x
+
+### Use in your code
+
+```javascript
+import {
+  CheckboxGroup,
+  InputLable,
+  InputRadio,
+  InputRange,
+  InputTag,
+  SearchList,
+  SearchTable,
+  SearchToolBar,
+} from "antd-component";
+```
